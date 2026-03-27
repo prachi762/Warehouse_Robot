fdc32a2e043ea3f69d942dd2c3f1e917bc9921ec
Prachi <prachi2006173@gmail.com>
2026-03-27 22:56:32 +0530

    Update state.py

--- core/state.py
+++ core/state.py
@@ -1,1 +1,24 @@
+class State:
+    def __init__(self, x, y, collected, energy, cost=0, parent=None):
+        self.x = x
+        self.y = y
+        self.collected = collected 
+        self.energy = energy
+        self.cost = cost            
+        self.parent = parent        
+
+    def __hash__(self):
+        return hash((self.x, self.y, self.collected, self.energy))
+
+    def __eq__(self, other):
+        return (self.x, self.y, self.collected, self.energy) == \
+               (other.x, other.y, other.collected, other.energy)
+def is_goal(state, k, dx, dy):
+    return state.collected == (1 << k) - 1 and state.x == dx and state.y == dy
+def reconstruct_path(state):
+    path = []
+    while state:
+        path.append((state.x, state.y))
+        state = state.parent
+    return path[::-1]
 
