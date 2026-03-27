5eaee19ca2dab84feed00f698852eb1e6fb81044
Prachi <prachi2006173@gmail.com>
2026-03-27 23:16:06 +0530

    Update manhattan.py

--- heuristics/manhattan.py
+++ heuristics/manhattan.py
@@ -1,1 +1,15 @@
+def manhattan(state, dx, dy):
+    """
+    Manhattan-to-delivery heuristic
+
+    h(n) = |x - x_d| + |y - y_d|
+
+    Parameters:
+        state : State object
+        dx, dy : delivery location
+
+    Returns:
+        heuristic value (int)
+    """
+    return abs(state.x - dx) + abs(state.y - dy)
 
