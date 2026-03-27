fe9fc8ad19400a0418f2c4018083a4e7f7f33475
Prachi <prachi2006173@gmail.com>
2026-03-27 23:02:42 +0530

    Update grid.py

--- core/grid.py
+++ core/grid.py
@@ -1,3 +1,5 @@
+from core.state import State
+
 class Grid:
     def __init__(self, N):
         self.N = N
@@ -7,19 +9,22 @@ class Grid:
         self.delivery = (0,0)
         self.start = (0,0)
         self.energy = 100
+
     def is_valid(self, x, y):
         return 0 <= x < self.N and 0 <= y < self.N and self.grid[x][y] == 0
-          def get_neighbors(self, state):
+
+    def get_neighbors(self, state):
         moves = [(1,0),(-1,0),(0,1),(0,-1)]
         neighbors = []
+
+        # If no energy, no moves possible
+        if state.energy <= 0:
+            return neighbors
 
         for dx, dy in moves:
             nx, ny = state.x + dx, state.y + dy
 
             if not self.is_valid(nx, ny):
-                continue
-
-            if state.energy <= 0:
                 continue
 
             new_collected = state.collected
@@ -41,5 +46,3 @@ class Grid:
             neighbors.append(new_state)
 
         return neighbors
-
-
