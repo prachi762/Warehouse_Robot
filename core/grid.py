3009cadbe62b32e14dbc8a106e2bfe1a7f1699cb
Prachi <prachi2006173@gmail.com>
2026-03-27 22:59:47 +0530

    Update grid.py

--- core/grid.py
+++ core/grid.py
@@ -1,1 +1,45 @@
+class Grid:
+    def __init__(self, N):
+        self.N = N
+        self.grid = [[0]*N for _ in range(N)]
+
+        self.items = []      # [(x,y), ...]
+        self.delivery = (0,0)
+        self.start = (0,0)
+        self.energy = 100
+    def is_valid(self, x, y):
+        return 0 <= x < self.N and 0 <= y < self.N and self.grid[x][y] == 0
+          def get_neighbors(self, state):
+        moves = [(1,0),(-1,0),(0,1),(0,-1)]
+        neighbors = []
+
+        for dx, dy in moves:
+            nx, ny = state.x + dx, state.y + dy
+
+            if not self.is_valid(nx, ny):
+                continue
+
+            if state.energy <= 0:
+                continue
+
+            new_collected = state.collected
+
+            # check item pickup
+            for i, (ix, iy) in enumerate(self.items):
+                if ix == nx and iy == ny:
+                    new_collected |= (1 << i)
+
+            new_state = State(
+                nx,
+                ny,
+                new_collected,
+                state.energy - 1,
+                state.cost + 1,
+                state
+            )
+
+            neighbors.append(new_state)
+
+        return neighbors
+
 
