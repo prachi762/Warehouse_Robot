e4a95cd245eacee7e0d42b2f4c21d6f61fd7b9e9
Prachi <prachi2006173@gmail.com>
2026-03-29 11:42:07 +0530

    Update easy_cases.py

--- demo_cases/easy_cases.py
+++ demo_cases/easy_cases.py
@@ -1,1 +1,19 @@
+from core.grid import Grid
+
+def easy_case_1():
+    g = Grid(10)
+    g.start = (0,0)
+    g.delivery = (9,9)
+    g.set_items([(2,2), (5,5)])
+    g.energy = 50
+    return g
+
+
+def easy_case_2():
+    g = Grid(8)
+    g.start = (0,0)
+    g.delivery = (7,7)
+    g.set_items([(3,3)])
+    g.energy = 30
+    return g
 
