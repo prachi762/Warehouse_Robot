8b2a7037e5b712dfe791d8a3ad548d2149c49416
Prachi <prachi2006173@gmail.com>
2026-03-29 16:29:26 +0530

    Update item settings in easy_case functions

--- demo_cases/easy_cases.py
+++ demo_cases/easy_cases.py
@@ -4,7 +4,10 @@ def easy_case_1():
     g = Grid(10)
     g.start = (0,0)
     g.delivery = (9,9)
-    g.set_items([(2,2), (5,5)])
+    g.set_items([
+        (2,2,5),     
+        (5,5,20)    
+    ])
     g.energy = 50
     return g
 
@@ -13,7 +16,9 @@ def easy_case_2():
     g = Grid(8)
     g.start = (0,0)
     g.delivery = (7,7)
-    g.set_items([(3,3)])
+    g.set_items([
+        (3,3,10)
+    ])
     g.energy = 30
     return g
 
