40bdd274373e721e0997eeda257f337db989fc33
Prachi <prachi2006173@gmail.com>
2026-03-29 16:22:32 +0530

    Add value parameter to Pareto functions

--- utils/pareto.py
+++ utils/pareto.py
@@ -1,18 +1,18 @@
-def is_dominated(pareto_list, cost, energy):
+def is_dominated(pareto_list, cost, energy, value):
 
-    for c, e in pareto_list:
-        if c <= cost and e >= energy:
+    for c, e, v in pareto_list:
+        if c <= cost and e >= energy and v >= value:
             return True
     return False
 
 
-def update_pareto(pareto_list, cost, energy):
+def update_pareto(pareto_list, cost, energy, value):
 
     new_list = []
 
-    for c, e in pareto_list:
-        if not (cost <= c and energy >= e):
-            new_list.append((c, e))
+    for c, e, v in pareto_list:
+        if not (cost <= c and energy >= e and value >= v):
+            new_list.append((c, e, v))
 
-    new_list.append((cost, energy))
+    new_list.append((cost, energy, value))
     return new_list
