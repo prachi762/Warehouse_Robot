c5e07ba59cd561c0e4ef059ad491247da2809fef
Prachi <prachi2006173@gmail.com>
2026-03-29 15:59:47 +0530

    Update bfs.py

--- algorithms/bfs.py
+++ algorithms/bfs.py
@@ -1,12 +1,15 @@
 from collections import deque
 from core.state import reconstruct_path
+from utils.pareto import is_dominated, update_pareto
+
 
 def bfs(grid, start_state):
 
     queue = deque([start_state])
-    visited = set()
 
-    visited.add(start_state)   
+    pareto = {}
+    key = (start_state.x, start_state.y, start_state.collected)
+    pareto[key] = [(start_state.cost, start_state.energy)]
 
     nodes_expanded = 0
 
@@ -15,14 +18,22 @@ def bfs(grid, start_state):
         nodes_expanded += 1
 
         if grid.is_goal(current):
-            path = reconstruct_path(current)
-            return path, nodes_expanded, current.cost
+            return reconstruct_path(current), nodes_expanded, current.cost
 
         for neighbor in grid.get_neighbors(current):
 
-            if neighbor not in visited:
-                visited.add(neighbor)
-                queue.append(neighbor)
+            c = neighbor.cost
+            e = neighbor.energy
+            key = (neighbor.x, neighbor.y, neighbor.collected)
+
+            if key not in pareto:
+                pareto[key] = [(c, e)]
+            else:
+                if is_dominated(pareto[key], c, e):
+                    continue
+
+                pareto[key] = update_pareto(pareto[key], c, e)
+
+            queue.append(neighbor)
 
     return None, nodes_expanded, float('inf')
-
