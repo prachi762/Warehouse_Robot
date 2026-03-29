db5b1af0830c2e0e6a33d73a184a9b4f34b41010
Prachi <prachi2006173@gmail.com>
2026-03-29 16:30:27 +0530

    Update item settings and grid configurations

--- demo_cases/medium_cases.py
+++ demo_cases/medium_cases.py
@@ -4,14 +4,19 @@ def medium_case_1():
     g = Grid(20)
     g.start = (0,0)
     g.delivery = (19,19)
-    g.set_items([(3,15), (10,10), (15,3)])
+
+    g.set_items([
+        (3,15,10),
+        (10,10,30),   
+        (15,3,15)
+    ])
+
     g.energy = 120
 
-    # vertical wall with gap
     for i in range(5,15):
         g.grid[i][8] = 1
 
-    g.grid[10][8] = 0  
+    g.grid[10][8] = 0
     return g
 
 
@@ -19,12 +24,16 @@ def medium_case_2():
     g = Grid(20)
     g.start = (0,0)
     g.delivery = (19,19)
-    g.set_items([(5,5), (10,15)])
+
+    g.set_items([
+        (5,5,10),
+        (10,15,25)
+    ])
+
     g.energy = 100
 
-    # horizontal wall
     for j in range(5,15):
         g.grid[10][j] = 1
 
-    g.grid[10][10] = 0  
+    g.grid[10][10] = 0
     return g
