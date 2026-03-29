0bd70aeffee13c88fc2dc97e0335314a585e0a68
Prachi <prachi2006173@gmail.com>
2026-03-29 15:56:42 +0530

    Update astar.py

--- algorithms/astar.py
+++ algorithms/astar.py
@@ -1,45 +1,49 @@
 import heapq
 from core.state import reconstruct_path
+from utils.pareto import is_dominated, update_pareto
+
 
 def astar(grid, start_state, heuristic):
 
     pq = []
-    counter = 0  
+    counter = 0
 
     start_h = heuristic(start_state)
     heapq.heappush(pq, (start_h, counter, start_state))
 
-    best_cost = {}
-    best_cost[start_state] = 0
+    pareto = {}
+    key = (start_state.x, start_state.y, start_state.collected)
+    pareto[key] = [(start_state.cost, start_state.energy)]
 
     nodes_expanded = 0
 
     while pq:
         f_val, _, current = heapq.heappop(pq)
-
-        g_val = current.cost
-
-        if g_val > best_cost[current]:
-            continue
-
         nodes_expanded += 1
 
         if grid.is_goal(current):
-            path = reconstruct_path(current)
-            return path, nodes_expanded, g_val
+            return reconstruct_path(current), nodes_expanded, current.cost
 
         for neighbor in grid.get_neighbors(current):
 
-            g_new = neighbor.cost
-            h_new = heuristic(neighbor)
+            h_val = heuristic(neighbor)
 
-            if h_new > neighbor.energy:
+            if h_val > neighbor.energy:
                 continue
 
-            if neighbor not in best_cost or g_new < best_cost[neighbor]:
-                best_cost[neighbor] = g_new
-                counter += 1
-                f_new = g_new + h_new
-                heapq.heappush(pq, (f_new, counter, neighbor))
+            key = (neighbor.x, neighbor.y, neighbor.collected)
+            c = neighbor.cost
+            e = neighbor.energy
+
+            if key not in pareto:
+                pareto[key] = [(c, e)]
+            else:
+                if is_dominated(pareto[key], c, e):
+                    continue
+
+                pareto[key] = update_pareto(pareto[key], c, e)
+
+            counter += 1
+            heapq.heappush(pq, (c + h_val, counter, neighbor))
 
     return None, nodes_expanded, float('inf')
