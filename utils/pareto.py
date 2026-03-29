70d683377ecf07add19fa8ef6ebc00155227e7c8
Prachi <prachi2006173@gmail.com>
2026-03-29 16:01:52 +0530

    Update pareto.py

--- utils/pareto.py
+++ utils/pareto.py
@@ -1,1 +1,18 @@
+def is_dominated(pareto_list, cost, energy):
 
+    for c, e in pareto_list:
+        if c <= cost and e >= energy:
+            return True
+    return False
+
+
+def update_pareto(pareto_list, cost, energy):
+
+    new_list = []
+
+    for c, e in pareto_list:
+        if not (cost <= c and energy >= e):
+            new_list.append((c, e))
+
+    new_list.append((cost, energy))
+    return new_list
