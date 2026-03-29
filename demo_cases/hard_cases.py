f7cc3ad6da1f9d3cfa67d6574c6b7815f820341f
Prachi <prachi2006173@gmail.com>
2026-03-29 16:31:35 +0530

    Update item settings in hard_case functions

--- demo_cases/hard_cases.py
+++ demo_cases/hard_cases.py
@@ -4,14 +4,20 @@ def hard_case_1():
     g = Grid(40)
     g.start = (0,0)
     g.delivery = (39,39)
-    g.set_items([(5,35),(10,10),(20,30),(30,5)])
+
+    g.set_items([
+        (5,35,10),
+        (10,10,50),   
+        (20,30,20),
+        (30,5,15)
+    ])
+
     g.energy = 200
 
-    # misleading wall
     for i in range(10,30):
         g.grid[i][20] = 1
 
-    g.grid[20][20] = 0 
+    g.grid[20][20] = 0
     return g
 
 
@@ -19,10 +25,15 @@ def hard_case_2():
     g = Grid(30)
     g.start = (0,0)
     g.delivery = (29,29)
-    g.set_items([(10,5),(5,20),(20,15)])
+
+    g.set_items([
+        (10,5,15),
+        (5,20,40),   
+        (20,15,20)
+    ])
+
     g.energy = 150
 
-    # maze-like structure
     for i in range(5,25):
         g.grid[i][10] = 1
         g.grid[i][20] = 1
