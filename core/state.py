63e1cb08125e32be7646a33113ad8546060e396d
Prachi <prachi2006173@gmail.com>
2026-03-29 18:49:39 +0530

    Simplify hash and equality methods in State class
    Remove energy from hash and equality comparisons.

--- core/state.py
+++ core/state.py
@@ -9,13 +9,13 @@ class State:
         self.value = value      
 
     def __hash__(self):
-        return hash((self.x, self.y, self.collected, self.energy))
+        return hash((self.x, self.y, self.collected))
 
     def __eq__(self, other):
         if not isinstance(other, State):
             return False
-        return (self.x, self.y, self.collected, self.energy) == \
-               (other.x, other.y, other.collected, other.energy)
+        return (self.x, self.y, self.collected) == \
+               (other.x, other.y, other.collected)
 
 def reconstruct_path(state):
     path = []
