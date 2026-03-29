9eaac08e44a951c03169fb129cc1aa8751e7c14b
Prachi <prachi2006173@gmail.com>
2026-03-29 16:32:32 +0530

    Update energy cases with item energy values

--- demo_cases/energy_cases.py
+++ demo_cases/energy_cases.py
@@ -4,9 +4,11 @@ def energy_case_1():
     g = Grid(15)
     g.start = (0,0)
     g.delivery = (14,14)
-    g.set_items([(7,7)])
 
-    # tight energy
+    g.set_items([
+        (7,7,50)  
+    ])
+
     g.energy = 20
     return g
 
@@ -15,8 +17,11 @@ def energy_case_2():
     g = Grid(20)
     g.start = (0,0)
     g.delivery = (19,19)
-    g.set_items([(10,10)])
 
-    # just enough energy
+    g.set_items([
+        (10,10,10),   
+        (15,15,40)   
+    ])
+
     g.energy = 40
     return g
