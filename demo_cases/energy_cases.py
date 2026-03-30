5c56bb4b26971366f570d120005b0b02c9c6f6c0
Prachi <prachi2006173@gmail.com>
2026-03-30 20:38:56 +0530

    Update energy_cases.py

--- demo_cases/energy_cases.py
+++ demo_cases/energy_cases.py
@@ -4,31 +4,26 @@ def energy_case_1():
     g = Grid(15)
     g.start = (0,0)
     g.delivery = (14,14)
-
     g.set_items([
         (7,7,50)  
     ])
+    g.energy = 1000 
 
-    g.energy = 45
-    
     for y in range(4, 7):
         for x in range(4, 7):
             g.grid[y][x] = 3
             
     return g
 
-
 def energy_case_2():
     g = Grid(20)
     g.start = (0,0)
     g.delivery = (19,19)
-
     g.set_items([
         (10,10,10),   
         (15,15,40)   
     ])
-
-    g.energy = 70
+    g.energy = 1200  
 
     for y in range(2, 18):
         for x in range(12, 14):
